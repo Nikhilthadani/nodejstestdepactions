03f668cdd8dea6bd93e6a60b78c85c0b8ab96338
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-04-26 17:24:12 +0530

    added route s

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   return res.status(200).json({ message: "Health Ok!" });
 });
-
+app.get("/api/home", (req, res) => {
+  return res.send("<h1>WELCOME TO PRODUCTION</h1");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("SERVER OPEN");
