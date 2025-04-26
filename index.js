d5f33eec08d7dace6677d3138fb9e8a6fe44185b
Nikhil Thadani <nikhilthadani112256@gmail.com>
2025-04-26 17:12:32 +0530

    added port default

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get("/health", (req, res) => {
   return res.status(200).json({ message: "Health Ok!" });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("SERVER OPEN");
 });
